cec32e48f0040f8efc0de6e08588541d83f6b623
SyedMuhgisHussain <syedmughis4541@gmail.com>
2023-07-08 16:08:31 +0500

    Chnages make in main.dart file to make web scoll smooth

--- lib/features/home/home.dart
+++ lib/features/home/home.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:web_app/features/aboutme/aboutme_widget.dart';
 import 'package:web_app/features/contactus/contact_us_widget.dart';
-//import 'package:web_app/features/intro/intro_widget.dart';
 import 'package:web_app/features/navbar/nav_bar.dart';
 import 'package:web_app/features/skills/what_i_do_widget.dart';
 import '../intro/intro_widget.dart';
 import '../projects/projects_widget.dart';
+import 'package:web_smooth_scroll/web_smooth_scroll.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -15,25 +15,41 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  late ScrollController _scrollController;
+
+  @override
+  void initState() {
+    _scrollController = ScrollController();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Column(
         children: [
-          NavBar(),
+          const NavBar(),
           Expanded(
-              child: SingleChildScrollView(
-                  child: Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 40),
-                      child: Column(
-                        children: [
-                          Intro(),
-                          AboutMe(),
-                          Skills(),
-                          Projects(),
-                          ContactMe(),
-                        ],
-                      )))),
+              child: WebSmoothScroll(
+            animationDuration: 600,
+            scrollOffset: 100,
+            curve: Curves.easeInOutCirc,
+            controller: _scrollController,
+            child: SingleChildScrollView(
+                physics: const NeverScrollableScrollPhysics(),
+                controller: _scrollController,
+                child: const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 40),
+                    child: Column(
+                      children: [
+                        Intro(),
+                        AboutMe(),
+                        Skills(),
+                        Projects(),
+                        ContactMe(),
+                      ],
+                    ))),
+          )),
         ],
       ),
     );
